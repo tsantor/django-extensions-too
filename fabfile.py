7b2357b4548a42b503ba41b22184931a4eb730d3
Tim Santor <tsantor@xstudiosinc.com>
2017-04-03 19:35:30 -0400

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from fabric.api import *
-from fabric.contrib.console import prompt
+from fabric.contrib.console import prompt, confirm
 
 version = __import__('django_extensions_too').__version__
 
@@ -8,14 +8,17 @@ def build():
     """Build and publish to PyPi."""
     local('python md2rst.py')
 
-    local('git add -A')
     message = prompt('Please supply a commit message')
-    local('git commit -m "{}"'.format(message))
-    local('git push origin master')
+    if message:
+        local('git add -A')
+        local('git commit -m "{}"'.format(message))
+        local('git push origin master')
 
-    local('git tag {}'.format(version))
-    local('git push origin --tags')
-    #local('pypi_publish')
+        local('git tag {}'.format(version))
+        local('git push origin --tags')
+
+    if confirm('Publish to PyPi'):
+        local('pypi_publish')
 
 @task
 def publish():
